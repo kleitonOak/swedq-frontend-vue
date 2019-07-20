1f0b18cf2d8286bb5424eab11f202eb6593c386c
José Kleiton da Silva <kleiton.contato@gmail.com>
2019-07-20 14:47:35 -0300

    Datatable component

---
+++ src/components/VehicleStatus.vue
@@ -0,0 +1,99 @@
+<template>
+   <div id="vehicle-status-table" class="app">
+      <mdb-datatable
+        :data="data"
+        striped
+        bordered
+        arrows
+        :display="3"
+        reactive
+        :time="7000"
+        refresh
+      />
+  </div>
+</template>
+
+<script>
+import { mdbDatatable, mdbContainer } from 'mdbvue';
+
+let rows = [
+              {
+                name: 'Tiger Nixon',
+                position: 'System Architect',
+                office: 'Edinburgh',
+                age: '61',
+                date: '2011/04/25',
+                salary: '$320'
+              },
+              {
+                name: 'Garrett Winters',
+                position: 'Accountant',
+                office: 'Tokyo',
+                age: '63',
+                date: '2011/07/25',
+                salary: '$170'
+              }
+            ];
+
+let columns = [
+              {
+                label: 'Name',
+                field: 'name',
+                sort: 'asc'
+              },
+              {
+                label: 'Position',
+                field: 'position',
+                sort: 'asc'
+              },
+              {
+                label: 'Office',
+                field: 'office',
+                sort: 'asc'
+              },
+              {
+                label: 'Age',
+                field: 'age',
+                sort: 'asc'
+              },
+              {
+                label: 'Start date',
+                field: 'date',
+                sort: 'asc'
+              },
+              {
+                label: 'Salary',
+                field: 'salary',
+                sort: 'asc'
+              }
+            ] 
+let dataDinanmic
+export default {
+  name: 'VehicleStatus',
+  props: {
+    msg: String
+  },
+  components: {
+      mdbDatatable,
+      mdbContainer
+  },
+  data() {
+      return {
+        columns: [],
+        rows: []
+      };
+  },
+  computed: {
+      data() {
+        return {
+          columns: columns,
+          rows: rows
+        };
+      },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
